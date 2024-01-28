e5646333c26a55d365898b7adaf4b0e38e1974eb
rhaylenee <119665308+rhaylenee@users.noreply.github.com>
2024-01-28 23:47:31 +0800

    Added ChatbotPrompt controller

---
+++ backend/Controllers/ChatbotPrompt.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace backend.Models
+{
+    public class ChatbotPrompt
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Category { get; set; } = string.Empty;
+
+        public string Question { get; set; } = string.Empty;
+
+        public string Answer {  get; set; } = string.Empty;
+      
+        [Column(TypeName = "datetime")]
+        public DateTime CreatdedAt { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime UpdatedAt {  get; set; }
+    }
+}
